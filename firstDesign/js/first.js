bec03c78abe14c51c5414f0b1adc23dfff278a7f
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-06-11 22:07:05 -0600

    testing. code from the three.js examples

--- firstDesign/js/first.js
+++ firstDesign/js/first.js
@@ -1,2 +1,12 @@
 const scene = new THREE.scene();
-const camer
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight, 
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
